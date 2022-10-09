df5271050090a1e2e2c143cc48e798c977290719
Regie <114920732+RegieSanJuan@users.noreply.github.com>
2022-10-09 18:21:50 +0800

    Add files via upload

---
+++ school 3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    float given_minutes = 125;
+    float hours = 125/60;
+    float minutes = 125%60;
+
+    printf("Given: %f\n", given_minutes );
+    printf("Converted hours: %f hours and %f minutes", hours, minutes); 
+    
+
+
+
+
+
+    return 0;
+
+
+
+}
